90c258a600013bbf3f7c30d5dfe1df7fad4d4657
vinicius <vinicius.soares.dev>
2022-11-18 19:35:45 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+const inputVerMais = document.querySelector('#verMais');
+const inputVerAntes = document.querySelector('#verAntes');
+const sectionTecnology = document.querySelector('.tecnologias');
+const sectionTecnology2 = document.querySelector('.tecnologias2');
+const frontInput = document.querySelector('.frontInput');
+const backInput = document.querySelector('.backInput');
+const gameInput = document.querySelector('.gameInput');
+const cardFuerza = document.querySelector('.cardFuerza');
+const cardBmi = document.querySelector('.cardBMI');
+const cardBookV = document.querySelector('.cardBookV');
+const cardTech = document.querySelector('.cardTech');
+const cardJokenpo = document.querySelector('.cardJokenpo');
+const cardQuizz = document.querySelector('.cardQuizz');
+
+frontInput.addEventListener('click', (event) => {
+    event.preventDefault();
+    frontInput.style.display = 'none';
+    gameInput.style.display = 'block';
+    backInput.style.display = 'block';
+    cardBmi.style.display = 'block';
+    cardFuerza.style.display = 'block';
+    cardBookV.style.display = 'none';
+    cardTech.style.display = 'none';
+    cardQuizz.style.display = 'none';
+    cardJokenpo.style.display = 'none';
+
+});
+
+backInput.addEventListener('click', (event) => {
+    event.preventDefault();
+    backInput.style.display = 'none';
+    gameInput.style.display = 'block';
+    frontInput.style.display = 'block';
+    cardBookV.style.display = 'block';
+    cardTech.style.display = 'block';
+    cardBmi.style.display = 'none';
+    cardFuerza.style.display = 'none';
+    cardQuizz.style.display = 'none';
+    cardJokenpo.style.display = 'none';
+
+});
+
+gameInput.addEventListener('click', (event) => {
+    event.preventDefault();
+    gameInput.style.display = 'none';
+    backInput.style.display = 'block';
+    frontInput.style.display = 'block';
+    cardBookV.style.display = 'none';
+    cardTech.style.display = 'none';
+    cardBmi.style.display = 'none';
+    cardFuerza.style.display = 'none';
+    cardQuizz.style.display = 'block';
+    cardJokenpo.style.display = 'block';
+
+});
+
+
+
+function screenDisplay() {
+    inputVerMais.addEventListener('click', (e) => {
+        e.preventDefault();
+    
+        sectionTecnology.style.display = 'none';
+        sectionTecnology2.style.display = 'block';
+    });
+    
+    inputVerAntes.addEventListener('click', (e) => {
+        e.preventDefault();
+    
+        sectionTecnology2.style.display = 'none';
+        sectionTecnology.style.display = 'block';
+    });
+
+}
+
+screenDisplay();
